a33218e4957be8ddc792d03e629feb7d4196bf0b
M A H D I <114954981+mahdiasli@users.noreply.github.com>
2023-06-28 23:11:56 +0330

    Add files via upload
    Ipfs task

--- IPSF_receive_photo.py
+++ IPSF_receive_photo.py
@@ -1,1 +1,15 @@
+import ipfshttpclient
 
+def download_image(image_hash):
+    client = ipfshttpclient.connect()  # برقراری ارتباط با گره IPFS محلی
+
+    try:
+        client.get(image_hash)  # دریافت عکس با استفاده از هش عکس
+        print("Image downloaded successfully.")
+    except Exception as e:
+        print(f"Error downloading image: {str(e)}")
+
+# هش عکس را که از ارسال کننده دریافت کرده‌اید، وارد کنید
+image_hash = 'QmWc3pwe6dZum4E8wpgiXJXDV6qwVCHuXgAep9MNLJKC4i'
+
+download_image(image_hash)
--- IPSF_send_photo.py
+++ IPSF_send_photo.py
@@ -1,1 +1,20 @@
+import ipfshttpclient
 
+def upload_image(image_path):
+    client = ipfshttpclient.connect()  # برقراری ارتباط با گره IPFS محلی
+
+    try:
+        res = client.add(image_path)  # ارسال عکس به IPFS
+        image_hash = res['Hash']  # دریافت هش عکس بارگذاری شده
+        return image_hash
+    except Exception as e:
+        print(f"Error uploading image: {str(e)}")
+        return None
+
+# مسیر فایل عکس را وارد کنید
+image_path = 'path_to_your_image.jpg'
+
+image_hash = upload_image(image_path)
+if image_hash:
+    print("Image uploaded successfully.")
+    print("Image Hash:", image_hash)
